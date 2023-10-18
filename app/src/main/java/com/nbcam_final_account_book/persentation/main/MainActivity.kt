edb731e1e7db31f9305a806125547c3886c9d8f0
ars-yeon <zosel7301@gmail.com>
2023-10-18 17:57:58 +0900

    bottom navigation view 및 toolbar 연결

--- app/src/main/java/com/nbcam_final_account_book/persentation/main/MainActivity.kt
+++ app/src/main/java/com/nbcam_final_account_book/persentation/main/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.nbcam_final_account_book.persentation.main
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.ViewPager2
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.nbcam_final_account_book.R
 import com.nbcam_final_account_book.databinding.MainActivityBinding
 import com.nbcam_final_account_book.persentation.entry.EntryActivity
@@ -10,9 +14,6 @@ import com.nbcam_final_account_book.persentation.entry.EntryActivity
 class MainActivity : AppCompatActivity() {
 
 	private lateinit var binding: MainActivityBinding
-	private val viewpagerAdapter by lazy {
-		MainViewpagerAdapter(this@MainActivity)
-	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -22,38 +23,33 @@ class MainActivity : AppCompatActivity() {
 		initView()
 	}
 
+	override fun onOptionsItemSelected(item: MenuItem): Boolean {
+		return when (item.itemId) {
+			android.R.id.home -> {
+				true
+			}
+			else -> super.onOptionsItemSelected(item)
+		}
+	}
+
 	private fun initView() = with(binding) {
-		mainViewpager.adapter = viewpagerAdapter
+		//toolbar 연결
+		setSupportActionBar(mainToolbar)
 
-//         뷰 페이저 스와이프 금지
-		mainViewpager.run {
-			isUserInputEnabled = false
-		}
-
-		//뷰 페이저 네비게이션바 연결
-		mainViewpager.registerOnPageChangeCallback(
-			object : ViewPager2.OnPageChangeCallback() {
-
-				override fun onPageSelected(position: Int) {
-					super.onPageSelected(position)
-					mainBottomNavi.menu.getItem(position).isChecked = true
-				}
-			}
+		//bottom navigation 연결
+		val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_fragment) as NavHostFragment
+		val navController = navHostFragment.navController
+//		val navController = findNavController(R.id.main_fragment)
+		val appBarConfiguration = AppBarConfiguration(
+			setOf(
+				R.id.menu_home, R.id.menu_statistics, R.id.menu_chat, R.id.menu_more
+			)
 		)
-
-		mainBottomNavi.setOnItemSelectedListener { menuItem ->
-			when (menuItem.itemId) {
-				R.id.menu_home -> mainViewpager.currentItem = 0
-				R.id.menu_statistics -> mainViewpager.currentItem = 1
-				R.id.menu_chat -> mainViewpager.currentItem = 2
-				R.id.menu_more -> mainViewpager.currentItem = 3
-			}
-
-			true
-		}
+		setupActionBarWithNavController(navController, appBarConfiguration)
+		mainBottomNavi.setupWithNavController(navController)
 
 		// 임시 fab 클릭 이벤트 (추후 삭제 or 수정 예정)
-		binding.fab.setOnClickListener {
+		fab.setOnClickListener {
 			val intent = EntryActivity.newIntent(this@MainActivity)
 			startActivity(intent)
 			overridePendingTransition(R.anim.slide_up_enter, R.anim.slide_up_exit)  // overrideActivityTransition 으로 변경 예정

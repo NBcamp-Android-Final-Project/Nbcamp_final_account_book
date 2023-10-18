bf3542b7e0b21b43026eda46193497cc84b325cd
송규진 <songu9610@gmail.com>
2023-10-18 18:00:35 +0900

    구글 로그인 추가

--- app/src/main/java/com/nbcam_final_account_book/persentation/login/LoginActivity.kt
+++ app/src/main/java/com/nbcam_final_account_book/persentation/login/LoginActivity.kt
@@ -5,8 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.nbcam_final_account_book.R
@@ -19,12 +29,24 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var binding: LoginActivityBinding
 
     private lateinit var auth: FirebaseAuth
+
+    private lateinit var launcher: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = LoginActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         initView()
+        launcher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+            ActivityResultCallback { result ->
+
+                if (result.resultCode == RESULT_OK) {
+                    val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                    googleSignInResult(task)
+                }
+
+            })
     }
 
     private fun initView() = with(binding) {
@@ -85,6 +107,20 @@ class LoginActivity : AppCompatActivity() {
 
         }
 
+        loginBtnGoogle.setOnClickListener {
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.google_login_default_url))
+                .requestEmail()
+                .build()
+
+            val googleSignInClient: GoogleSignInClient =
+                GoogleSignIn.getClient(this@LoginActivity, gso)
+
+            val signInIntent = googleSignInClient.signInIntent
+
+            launcher.launch(signInIntent)
+        }
+
 
     }
 
@@ -93,6 +129,35 @@ class LoginActivity : AppCompatActivity() {
         val intent = Intent(this@LoginActivity, MainActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    private fun googleSignInResult(task: Task<GoogleSignInAccount>) {
+        try {
+
+            val account = task.getResult(ApiException::class.java)
+            val idToken = account?.idToken
+
+            val credential = GoogleAuthProvider.getCredential(idToken, null)
+            auth.signInWithCredential(credential)
+                .addOnCompleteListener(this@LoginActivity) { firebaseTask ->
+                    if (firebaseTask.isSuccessful) {
+                        Toast.makeText(
+                            this@LoginActivity,
+                            "Google login successful",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                      toMainActivity()
+                    } else {
+                        Toast.makeText(
+                            this@LoginActivity,
+                            "Firebase login failed",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        } catch (e: ApiException) {
+            Toast.makeText(this@LoginActivity, "Google login failed", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }

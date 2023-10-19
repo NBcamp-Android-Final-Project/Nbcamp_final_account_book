87184987b69aadfdc1b69561ac2810dc20cb9ae5
송규진 <songu9610@gmail.com>
2023-10-19 11:52:45 +0900

    테스트 코드 작성중

--- app/src/main/java/com/nbcam_final_account_book/persentation/template/TemplateActivity.kt
+++ app/src/main/java/com/nbcam_final_account_book/persentation/template/TemplateActivity.kt
@@ -3,6 +3,8 @@ package com.nbcam_final_account_book.persentation.template
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
@@ -15,7 +17,7 @@ class TemplateActivity : AppCompatActivity() {
 
     private lateinit var binding: TemplateActivityBinding
     private lateinit var viewModel: TemplateViewModel
-    private var isFirst = true
+    private val isFirst = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +31,28 @@ class TemplateActivity : AppCompatActivity() {
 
     private fun initView() = with(binding) {
 
+        templateEdtInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+                val text = s.toString()
+
+                if (isEnable(text)) {
+                    templateTvError.text = "불가능"
+                } else {
+                    templateTvError.text = "가능"
+                }
+            }
+
+        })
+
         templateBtnOk.setOnClickListener {
 
             val templateTitle = templateEdtInput.text.toString()
@@ -36,14 +60,14 @@ class TemplateActivity : AppCompatActivity() {
             if (isEnable(templateTitle)) {
                 Toast.makeText(
                     this@TemplateActivity,
-                    "금지된 문자가 포함되어 있습니다. : '#' , '[' , ']' , '.' , '$' ",
+                    "금지된 문자가 포함되어 있습니다.",
                     Toast.LENGTH_LONG
                 )
                     .show()
             } else {
 
                 saveIsFirst(isFirst)
-//                toMainActivity()
+                toMainActivity()
             }
 
 

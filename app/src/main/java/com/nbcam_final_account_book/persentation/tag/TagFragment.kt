23596ebd3cf7546161c9e71c01e12a1684516d86
KotDev99 <KotDev99@gmail.com>
2023-10-19 18:06:27 +0900

    tag_fragment.xml /> 누락 및 more 에서 tag 네비게이션 이동

--- app/src/main/java/com/nbcam_final_account_book/persentation/tag/TagFragment.kt
+++ app/src/main/java/com/nbcam_final_account_book/persentation/tag/TagFragment.kt
@@ -1,45 +1,47 @@
 package com.nbcam_final_account_book.persentation.tag
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.nbcam_final_account_book.databinding.TagFragmentBinding
 
 
 class TagFragment : Fragment() {
-    private var _binding: TagFragmentBinding? = null
-    private val binding get() = _binding!!
-    private val viewModel by lazy {
-        ViewModelProvider(
-            this@TagFragment
-        )[TagViewModel::class.java]
-    }
+	private var _binding: TagFragmentBinding? = null
+	private val binding get() = _binding!!
+	private val viewModel by lazy {
+		ViewModelProvider(
+			this@TagFragment
+		)[TagViewModel::class.java]
+	}
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = TagFragmentBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		_binding = TagFragmentBinding.inflate(inflater, container, false)
+		return binding.root
+	}
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
 
-        initView()
-        initViewModel()
-    }
+		initView()
+		initViewModel()
+	}
 
-    private fun initView() = with(binding) {
+	private fun initView() = with(binding) {
+		ivBack.setOnClickListener {
+			// 뒤로 가기
+		}
+	}
 
-    }
+	private fun initViewModel() = with(viewModel) {
+		liveDummyTagInTag.observe(viewLifecycleOwner) {
 
-    private fun initViewModel() = with(viewModel) {
-        liveDummyTagInTag.observe(viewLifecycleOwner) {
-
-        }
-    }
+		}
+	}
 }

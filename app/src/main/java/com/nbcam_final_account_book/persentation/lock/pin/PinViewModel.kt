c37b8989a4aa9db3178f7963bf319fb2811f60d3
ars-yeon <zosel7301@gmail.com>
2023-10-20 19:32:03 +0900

    password → pin

--- app/src/main/java/com/nbcam_final_account_book/persentation/lock/pin/PinViewModel.kt
+++ app/src/main/java/com/nbcam_final_account_book/persentation/lock/pin/PinViewModel.kt
@@ -1,5 +1,6 @@
 package com.nbcam_final_account_book.persentation.lock.pin
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.nbcam_final_account_book.data.sharedprovider.SharedProvider
@@ -7,24 +8,27 @@ import com.nbcam_final_account_book.data.sharedprovider.SharedProvider
 class PinViewModel(sharedProvider: SharedProvider): ViewModel() {
 
     companion object {
-        const val APP_LOCK_PASSWORD = "AppLockPassword"
-        const val LOCK_PASSWORD = "lockPassword"
+        const val PIN_VIEW_MODEL = "PinViewModel"
+        const val APP_LOCK_PIN = "AppLockPin"
+        const val LOCK_PIN = "lockPin"
     }
 
-    private val sharedPreferences = sharedProvider.setSharedPref(APP_LOCK_PASSWORD)
+    private val sharedPreferences = sharedProvider.setSharedPref(APP_LOCK_PIN)
 
-    fun arePinMatching(password1: String, password2: String): Boolean {
-        return password1 == password2
+    fun arePinMatching(pin1: String, pin2: String): Boolean {
+        return pin1 == pin2
     }
 
-    fun savePin(password: String) {
+    fun savePin(pin: String) {
         val editor = sharedPreferences.edit()
-        editor.putString(LOCK_PASSWORD, password)
+        editor.putString(LOCK_PIN, pin)
         editor.apply()
+
+        Log.d(PIN_VIEW_MODEL, "저장된 비밀번호: $pin")
     }
 
-    fun getSavedPassword(): String? {
-        return sharedPreferences.getString(LOCK_PASSWORD, null)
+    fun getSavedPin(): String? {
+        return sharedPreferences.getString(LOCK_PIN, null)
     }
 }
 

9ba8a675a45c14a696e7abf6868c34ccc5015bb7
송규진 <songu9610@gmail.com>
2023-10-20 20:39:34 +0900

    room에 list를 json으로 받을 수 있도록 1차 설계

--- app/src/main/java/com/nbcam_final_account_book/data/room/AndroidRoomDataBase.kt
+++ app/src/main/java/com/nbcam_final_account_book/data/room/AndroidRoomDataBase.kt
@@ -4,20 +4,24 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.nbcam_final_account_book.data.model.local.TemplateDataEntity
 import com.nbcam_final_account_book.data.model.local.TemplateEntity
+import com.nbcam_final_account_book.data.room.dao.TemplateDao
+import com.nbcam_final_account_book.data.room.dao.TemplateDataDao
 
-@Database(entities = [TemplateEntity::class], version = 2)
+@Database(entities = [TemplateEntity::class], [TemplateDataEntity::class], version = 3)
 abstract class AndroidRoomDataBase : RoomDatabase() {
 
     abstract fun templateDao(): TemplateDao
+    abstract fun templateDataDao(): TemplateDataDao
 
     companion object {
         @Volatile
         private var INSTANCE: AndroidRoomDataBase? = null
 
         fun getInstance(
-            context:Context
-        ):AndroidRoomDataBase{
+            context: Context
+        ): AndroidRoomDataBase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,

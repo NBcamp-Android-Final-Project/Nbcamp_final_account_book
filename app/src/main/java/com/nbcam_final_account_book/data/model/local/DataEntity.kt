5f5392d93d21071de1691f6db032c0ff20094d24
송규진 <songu9610@gmail.com>
2023-10-23 09:22:28 +0900

    템플릿 데이터 저장을 위한 room 설계

---
+++ app/src/main/java/com/nbcam_final_account_book/data/model/local/DataEntity.kt
@@ -0,0 +1,18 @@
+package com.nbcam_final_account_book.data.model.local
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "data_entity")
+data class DataEntity(
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "data_id")
+    val id: Int = 0,
+    @ColumnInfo("data_entryList")
+    val entryList: String = "",
+    @ColumnInfo(name = "data_tagList")
+    val tagList: String = "",
+    @ColumnInfo(name = "data_budgetList")
+    val budgetList: String = ""
+)
---
+++ app/src/main/java/com/nbcam_final_account_book/data/room/dao/DataDao.kt
@@ -0,0 +1,7 @@
+package com.nbcam_final_account_book.data.room.dao
+
+import androidx.room.Dao
+
+@Dao
+interface DataDao {
+}

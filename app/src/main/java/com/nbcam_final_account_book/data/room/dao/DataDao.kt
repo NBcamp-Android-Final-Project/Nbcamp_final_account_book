e50507a8d51de7013aafbc02484aa3bdef3ca114
송규진 <songu9610@gmail.com>
2023-10-23 18:56:28 +0900

    (onConflict = OnConflictStrategy.IGNORE) 추가로 중복 키 방어

--- app/src/main/java/com/nbcam_final_account_book/data/room/dao/DataDao.kt
+++ app/src/main/java/com/nbcam_final_account_book/data/room/dao/DataDao.kt
@@ -2,6 +2,7 @@ package com.nbcam_final_account_book.data.room.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.nbcam_final_account_book.data.model.local.DataEntity
@@ -13,7 +14,7 @@ interface DataDao {
     @Query("SELECT * FROM data_table WHERE data_id = :id")
     suspend fun getDataById(id: String): DataEntity? // id = TemplateEntity.id
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertData(entity: DataEntity) // DataEntity.id = TemplateEntity.id
 
     @Update

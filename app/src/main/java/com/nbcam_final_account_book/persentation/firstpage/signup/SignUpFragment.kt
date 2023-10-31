b7e9986baefbe89346dfd62a070b5f69d6367394
송규진 <songu9610@gmail.com>
2023-10-31 16:50:41 +0900

    프래그먼트 이름 변경에 따른 binding 변수 변경

--- app/src/main/java/com/nbcam_final_account_book/persentation/firstpage/signup/SignUpFragment.kt
+++ app/src/main/java/com/nbcam_final_account_book/persentation/firstpage/signup/SignUpFragment.kt
@@ -5,18 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.nbcam_final_account_book.databinding.SignUpFragmentBinding
+import com.nbcam_final_account_book.databinding.FirstSignUpFragmentBinding
 
 class SignUpFragment : Fragment() {
 
-    private var _binding: SignUpFragmentBinding? = null
+    private var _binding: FirstSignUpFragmentBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = SignUpFragmentBinding.inflate(inflater, container, false)
+        _binding = FirstSignUpFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 }

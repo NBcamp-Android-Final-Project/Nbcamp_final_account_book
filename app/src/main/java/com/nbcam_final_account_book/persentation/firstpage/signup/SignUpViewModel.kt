5d8c076b3ba9749d12bbbdc2695803ce3d8427ee
송규진 <songu9610@gmail.com>
2023-11-01 11:13:30 +0900

    회원가입 로직이 포함된 viewModel구성.

---
+++ app/src/main/java/com/nbcam_final_account_book/persentation/firstpage/signup/SignUpViewModel.kt
@@ -0,0 +1,34 @@
+package com.nbcam_final_account_book.persentation.firstpage.signup
+
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+class SignUpViewModel() : ViewModel() {
+
+    private lateinit var auth: FirebaseAuth
+
+    suspend fun signUp(email: String, password: String, name: String): Boolean =
+        withContext(Dispatchers.IO) {
+           try {
+                val result = auth.createUserWithEmailAndPassword(email, password).await()
+
+                val user = auth.currentUser
+                val profileUpdates = userProfileChangeRequest {
+                    displayName = name
+                }
+                user?.updateProfile(profileUpdates)?.await()
+                true
+            } catch (e: Exception) {
+
+                false
+            }
+        }
+
+}

2e18d1325c195c0508b3dfd14f51bf646f1c6997
송규진 <songu9610@gmail.com>
2023-11-06 10:33:40 +0900

    userdata crud를 위한 dao추가

--- app/src/main/java/com/nbcam_final_account_book/data/room/dao/UserDataDao.kt
+++ app/src/main/java/com/nbcam_final_account_book/data/room/dao/UserDataDao.kt
@@ -1,5 +1,6 @@
 package com.nbcam_final_account_book.data.room.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -10,12 +11,25 @@ import com.nbcam_final_account_book.data.model.local.UserDataEntity
 @Dao
 interface UserDataDao {
 
+    //Insert
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertUserData(user: UserDataEntity)
 
+    //SELECT
+    @Query("SELECT * FROM user_data_table")
+    fun getALlUserList(): List<UserDataEntity>
+
+    @Query("SELECT * FROM user_data_table")
+    fun getALlUserLiveData(): LiveData<List<UserDataEntity>>
+
+    @Query("SELECT * FROM user_data_table WHERE user_uid = :key")
+    suspend fun getUserByKey(key: String): UserDataEntity
+
+    //DELETE
     @Query("DELETE FROM user_data_table WHERE user_uid = :key")
     suspend fun deleteUser(key: String)
 
+    //Update
     @Update
     suspend fun updateUser(user: UserDataEntity)
 }

1992f84ee3e79fd0ff025cf87346974b58f41775
ars-yeon <zosel7301@gmail.com>
2023-11-24 12:23:58 +0900

    MyPageWithdrawDialog 코드 리팩토링
    - DialogFragment 상속
    - ViewBinding 사용

--- app/src/main/java/com/nbcam_final_account_book/persentation/mypage/mypagedialog/MyPageWithdrawDialog.kt
+++ app/src/main/java/com/nbcam_final_account_book/persentation/mypage/mypagedialog/MyPageWithdrawDialog.kt
@@ -1,28 +1,83 @@
 package com.nbcam_final_account_book.persentation.mypage.mypagedialog
 
+import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.view.Window
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
 import com.nbcam_final_account_book.R
+import com.nbcam_final_account_book.databinding.MyPageWithdrawDialogBinding
 
-class MyPageWithdrawDialog(context: Context, onWithdrawConfirmed: () -> Unit) {
-    private val dialog: AlertDialog
+class MyPageWithdrawDialog(private val onWithdrawConfirmed: () -> Unit) : DialogFragment() {
 
-    init {
-        val builder = AlertDialog.Builder(context, R.style.EditNameAlertDialogStyle)
-        val customView = View.inflate(context, R.layout.my_page_withdraw_dialog, null)
+    private var _binding: MyPageWithdrawDialogBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var mContext: Context
+    private lateinit var alertDialog: AlertDialog
 
-        builder.apply {
-            setView(customView)
-            setPositiveButton("확인") { _, _ ->
-                onWithdrawConfirmed()
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = MyPageWithdrawDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        _binding = MyPageWithdrawDialogBinding.inflate(layoutInflater, null, false)
+        val dialog = Dialog(requireContext())
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(binding.root)
+        return dialog
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val width = (resources.displayMetrics.widthPixels * 0.85).toInt()
+        val height = ViewGroup.LayoutParams.WRAP_CONTENT
+        dialog?.window?.setLayout(width, height)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initView() = with(binding){
+        alertDialog = AlertDialog.Builder(mContext, R.style.EditNameAlertDialogStyle)
+            .setView(root)
+            .setNegativeButton("취소", null)
+            .setPositiveButton("저장", null)
+            .create().apply {
+                setCancelable(false)
             }
-            setNegativeButton("취소") { dialog, _ ->
-                dialog.dismiss()
-            }
+
+        tvDialogCancel.setOnClickListener {
+            dismiss()
         }
 
-        dialog = builder.create()
-        dialog.show()
+        tvDialogOkay.setOnClickListener {
+            onWithdrawConfirmed()
+            dismiss()
+        }
     }
 }
